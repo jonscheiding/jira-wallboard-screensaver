b1085916030fab70251424d47fb2624a83cece98
Jon Scheiding <jonscheiding@gmail.com>
2017-08-28 14:06:36 -0400

    #3 Created a tests assembly with a simple smoke test

---
+++ Jira.WallboardScreensaver.Tests/SmokeTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Jira.WallboardScreensaver.Tests
+{
+    [TestFixture]
+    public class SmokeTests
+    {
+        [Test]
+        public void BuildSystemTestShouldAlwaysPass()
+        {
+            Console.WriteLine("This test is just here to see if the testing system works.");
+        }
+    }
+}
