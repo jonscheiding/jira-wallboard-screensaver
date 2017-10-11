00170eb7fd983601e8db81be98560d0c05593139
Jon Scheiding <jscheiding@certicasolutions.com>
2017-10-11 14:37:02 -0400

    #9 Error service implementation

--- Jira.WallboardScreensaver/Services/ErrorMessageService.cs
+++ Jira.WallboardScreensaver/Services/ErrorMessageService.cs
@@ -7,8 +7,19 @@ namespace Jira.WallboardScreensaver.Services {
     }
 
     public class ErrorMessageService : IErrorMessageService {
+        class Owner : IWin32Window {
+            public IntPtr Handle { get; set; }
+        }
+
         public void ShowErrorMessage(object view, string message, string title) {
-            throw new NotImplementedException();
+            var form = view as Form;
+
+            if (form != null) {
+                MessageBox.Show(new Owner {Handle = form.Handle},
+                    message, title, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            } else {
+                MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
